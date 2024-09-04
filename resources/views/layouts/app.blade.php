c447b0110bccee3ab25839f4632e2dc754815ea6
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-09-04 10:38:13 -0600

    spatie

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,7 @@
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('cliente.index') }}">Cliente</a>
+                            <a class="nav-link" href="{{ route('cliente.index') }}"></a>
                         </li>
                     </ul>
 
