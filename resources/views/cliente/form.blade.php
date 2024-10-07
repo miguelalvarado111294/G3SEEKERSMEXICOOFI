cfc8ec527eefc3e94319a5b357d144d64cef96d8
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-10-07 10:52:45 -0600

    ya se ven las imagenes y edit igual

--- resources/views/cliente/form.blade.php
+++ resources/views/cliente/form.blade.php
@@ -1,188 +1,137 @@
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="nombre">Nombre</label>
-            <input type="text" class="form-control" name="nombre"
-                value="{{ isset($cliente->nombre) ? $cliente->nombre : old('nombre') }}" id="nombre">
-            @error('nombre')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
+<div class="form-group">
+    <label for="nombre">Nombre</label>
+    <input type="text" class="form-control" name="nombre"
+        value="{{ isset($cliente->nombre) ? $cliente->nombre : old('nombre') }}" id="nombre">
+    @error('nombre')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="segnombre">Segundo Nombre</label>
+    <input type="text" class="form-control" name="segnombre"
+        value="{{ isset($cliente->segnombre) ? $cliente->segnombre : old('segnombre') }}" id="segnombre">
+    @error('segnombre')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="apellidopat">Apellido Paterno</label>
+    <input type="text" class="form-control" name="apellidopat"
+        value="{{ isset($cliente->apellidopat) ? $cliente->apellidopat : old('apellidopat') }}" id="apellidopat">
+    @error('apellidopat')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+    <br>
+</div>
+
+<div class="form-group">
+    <label for="apellidomat">Apellido Materno</label>
+    <input type="text" class="form-control" name="apellidomat"
+        value="{{ isset($cliente->apellidomat) ? $cliente->apellidomat : old('apellidomat') }}" id="apellidomat">
+    @error('apellidomat')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="telefono">Telefono</label>
+    <input type="text" class="form-control" name="telefono"
+        value="{{ isset($cliente->telefono) ? $cliente->telefono : old('telefono') }}" id="telefono">
+    @error('telefono')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="direccion">Direccion</label>
+    <input type="text" class="form-control" name="direccion"
+        value="{{ isset($cliente->direccion) ? $cliente->direccion : old('direccion') }}" id="direccion"
+        placeholder="Calle /Numero /Colonia /Ciudad">
+    @error('direccion')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="email">Email</label>
+    <input type="text" class="form-control" name="email"
+        value="{{ isset($cliente->email) ? $cliente->email : old('email') }}" id="email">
+    @error('email')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="rfc">RFC</label>
+    <input type="text" class="form-control" name="rfc"
+        value="{{ isset($cliente->rfc) ? $cliente->rfc : old('rfc') }}" id="rfc">
+    @error('rfc')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="actaconstitutiva">Acta Constitutiva</label>
+    <img src="{{ asset('storage/') . '/' . $cliente->actaconstitutiva }}" width="200" alt="">
+
+    <input type="file" class="form-control" name="actaconstitutiva"
+        value="{{ isset($cliente->actaconstitutiva) ? $cliente->actaconstitutiva : old('actaconstitutiva') }}"
+        accept="image/*" id="actaconstitutiva">
+    @error('actaconstitutiva')
+        <small style ="color: red"> {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="consFiscal">Constancia de Situación Fiscal</label>
+    <img src="{{ asset('storage/') . '/' . $cliente->consFiscal }}" width="200" alt="">
+    <input type="file" class="form-control" name="consFiscal"
+        value="{{ isset($cliente->consFiscal) ? $cliente->consFiscal : old('consFiscal') }} " id="consFiscal">
+    @error('consFiscal')
+        <small style ="color: red">
+            {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="comprDom">Comprobante de Domicilio</label>
+    <img src="{{ asset('storage/') . '/' . $cliente->comprDom }}" width="200" alt="">
+    <input type="file" class="form-control" name="comprDom"
+        value="{{ isset($cliente->comprDom) ? $cliente->comprDom : old('comprDom') }}" id="comprDom">
+    @error('comprDom')
+        <small style ="color: red">
+            {{ $message }}</small>
+    @enderror
+</div>
+
+<div class="form-group">
+    <label for="tarjetacirculacion">Tarjeta de Circulacion</label>
+    <img src="{{ asset('storage/') . '/' . $cliente->tarjetacirculacion }}" width="200" alt="">
+
+    <input type="file" class="form-control" name="tarjetacirculacion"
+        value="{{ isset($cliente->tarjetacirculacion) ? $cliente->tarjetacirculacion : old('tarjetacirculacion') }} "
+        id="tarjetacirculacion">
+    @error('tarjetacirculacion')
+        <small style ="color: red">
+            {{ $message }}</small>
+    @enderror
 </div>
 
 
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="segnombre">Segundo Nombre</label>
-            <input type="text" class="form-control" name="segnombre"
-                value="{{ isset($cliente->segnombre) ? $cliente->segnombre : old('segnombre') }}" id="segnombre">
-            @error('segnombre')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
+
+<div class="form-group">
+    <label for="compPago">Comprobante de Pago </label>
+    <img src="{{ asset('storage/') . '/' . $cliente->compPago }}" width="200" alt="">
+    <input type="file" class="form-control" name="compPago"
+        value="{{ isset($cliente->compPago) ? $cliente->compPago : old('compPago') }} " id="compPago">
+    @error('compPago')
+        <small style ="color: red">
+            {{ $message }}</small>
+    @enderror
 </div>
 
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="apellidopat">Apellido Paterno</label>
-            <input type="text" class="form-control" name="apellidopat"
-                value="{{ isset($cliente->apellidopat) ? $cliente->apellidopat : old('apellidopat') }}"
-                id="apellidopat">
-            @error('apellidopat')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-            <br>
-        </div>
-    </div>
-</div>
-
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="apellidomat">Apellido Materno</label>
-            <input type="text" class="form-control" name="apellidomat"
-                value="{{ isset($cliente->apellidomat) ? $cliente->apellidomat : old('apellidomat') }}"
-                id="apellidomat">
-            @error('apellidomat')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="telefono">Telefono</label>
-            <input type="text" class="form-control" name="telefono"
-                value="{{ isset($cliente->telefono) ? $cliente->telefono : old('telefono') }}" id="telefono">
-            @error('telefono')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="direccion">Direccion</label>
-            <input type="text" class="form-control" name="direccion"
-                value="{{ isset($cliente->direccion) ? $cliente->direccion : old('direccion') }}" id="direccion"
-                placeholder="Calle /Numero /Colonia /Ciudad">
-            @error('direccion')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input type="text" class="form-control" name="email"
-                value="{{ isset($cliente->email) ? $cliente->email : old('email') }}" id="email">
-            @error('email')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="rfc">RFC</label>
-            <input type="text" class="form-control" name="rfc"
-                value="{{ isset($cliente->rfc) ? $cliente->rfc : old('rfc') }}" id="rfc">
-            @error('rfc')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="actaconstitutiva">Acta Constitutiva</label>
-            <input type="file" class="form-control" name="actaconstitutiva"
-                value="{{ isset($cliente->actaconstitutiva) ? $cliente->actaconstitutiva : old('actaconstitutiva') }}"
-                accept="image/*" id="actaconstitutiva">
-            @error('actaconstitutiva')
-                <small style ="color: red"> {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="consFiscal">Constancia de Situación Fiscal</label>
-            <input type="file" class="form-control" name="consFiscal"
-                value="{{ isset($cliente->consFiscal) ? $cliente->consFiscal : old('consFiscal') }} " id="consFiscal">
-            @error('consFiscal')
-                <small style ="color: red">
-                    {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="comprDom">Comprobante de Domicilio</label>
-            <input type="file" class="form-control" name="comprDom"
-                value="{{ isset($cliente->comprDom) ? $cliente->comprDom : old('comprDom') }}" id="comprDom">
-            @error('comprDom')
-                <small style ="color: red">
-                    {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="tarjetacirculacion">Tarjeta de Circulacion</label>
-            <input type="file" class="form-control" name="tarjetacirculacion"
-                value="{{ isset($cliente->tarjetacirculacion) ? $cliente->tarjetacirculacion : old('tarjetacirculacion') }} "
-                id="tarjetacirculacion">
-
-            @error('tarjetacirculacion')
-                <small style ="color: red">
-                    {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-sm-7">
-        <div class="form-group">
-            <label for="compPago">Comprobante de Pago </label>
-
-            <input type="file" class="form-control" name="compPago"
-                value="{{ isset($cliente->compPago) ? $cliente->compPago : old('compPago') }} " id="compPago">
-
-            @error('compPago')
-                <small style ="color: red">
-                    {{ $message }}</small>
-            @enderror
-        </div>
-    </div>
-</div>
 
 <div class="row">
     <div class="col-sm-7">
@@ -191,8 +140,6 @@
         </div>
     </div>
 </div>
-
-
 
 <div class="form-group">
     <a href="{{ URL::previous() }}" class="btn btn-dark">Regresar</a>
--- resources/views/registroCliente/datosreferencia.blade.php
+++ resources/views/registroCliente/datosreferencia.blade.php
@@ -3,7 +3,7 @@
 @section('content_header')
     <h1 class="text-center"><b>G3 Seekers</b></h1>
     <br>
-    <h3 class="text-center">Datos Personales</h3>
+    <h3 class="text-center">Registre una Referencia en Caso de Emergencia</h3>
     <br>
     <div class="card">
         <div class="card-body">
