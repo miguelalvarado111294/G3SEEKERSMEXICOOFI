fcee59cf952517ca88d47c67f13701e8d498b9ca
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-10-22 17:24:42 -0600

    roles editados

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -19,59 +19,67 @@ class RoleSeeder extends Seeder
     {
         //
         $role1 = Role::create(['name' => 'Admin']);
-        $role2 = Role::create(['name' => 'Monitoreo']); //no pueder ver o agregar 
-        $role3 = Role::create(['name' => 'Cobranza']);
+        $role2 = Role::create(['name' => 'Cobranza']); //no pueder ver o agregar 
+        $role3 = Role::create(['name' => 'Operaciones']);
         $role4 = Role::create(['name' => 'Contabilidad']);
-        $role5 = Role::create(['name' => 'Operaciones']);
+        $role5 = Role::create(['name' => 'Monitoreo']);
 
         //administracion
         Permission::create(['name' => 'admin.index'])->syncRoles([$role1]);
         Permission::create(['name' => 'admin.users'])->syncRoles([$role1]);
         Permission::create(['name' => 'admin.edit'])->syncRoles([$role1]);
+        Permission::create(['name' => 'usuarios.create'])->syncRoles([$role1]);
+        Permission::create(['name' => 'usuarios.store'])->syncRoles([$role1]);
+
+
 
         //cliente
-
-        Permission::create(['name' => 'cliente.index'])->syncRoles([$role1, $role2, $role3]);  //Cobranza    Monitoreo
-        Permission::create(['name' => 'cliente.show'])->syncRoles([$role1, $role2, $role3]);   //Cobranza   Monitoreo 
-        Permission::create(['name' => 'cliente.create'])->syncRoles([$role1, $role3]);  //Cobranza 
-        Permission::create(['name' => 'cliente.edit'])->syncRoles([$role1, $role3]);  //Cobranza 
-        Permission::create(['name' => 'cliente.destroy'])->syncRoles([$role1]);  //administrador 
-        //refeerencias
-
-        Permission::create(['name' => 'referencia.index'])->syncRoles([$role1, $role2]); //Cobranza  
-
-        Permission::create(['name' => 'referencia.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'referencia.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
+        Permission::create(['name' => 'cliente.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]);
+        
+        //referencia
+        Permission::create(['name' => 'referencia.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza  
+        Permission::create(['name' => 'referencia.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'referencia.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
         Permission::create(['name' => 'referencia.destroy'])->syncRoles([$role1]);
+        
         //cuenta
-
-        Permission::create(['name' => 'cuenta.index'])->syncRoles([$role1, $role2, $role3]); //Cobranza 
-        Permission::create(['name' => 'cuenta.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'cuenta.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
+        Permission::create(['name' => 'cuenta.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza 
+        Permission::create(['name' => 'cuenta.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'cuenta.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
         Permission::create(['name' => 'cuenta.destroy'])->syncRoles([$role1]);
+
         //vehiculo
-
-        Permission::create(['name' => 'vehiculo.index'])->syncRoles([$role1, $role2 ,$role3]); //Cobranza 
-        Permission::create(['name' => 'vehiculo.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'vehiculo.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
+        Permission::create(['name' => 'vehiculo.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza 
+        Permission::create(['name' => 'vehiculo.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'vehiculo.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
         Permission::create(['name' => 'vehiculo.destroy'])->syncRoles([$role1]);
+
         //dispositivo
-        //operaciones para generar formato de servicio
-        Permission::create(['name' => 'dispositivo.index'])->syncRoles([$role1, $role2, $role3]); //Cobranza 
-        Permission::create(['name' => 'dispositivo.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'dispositivo.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
-        Permission::create(['name' => 'dispositivo.destroy'])->syncRoles([$role1, $role3]);
+        Permission::create(['name' => 'dispositivo.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza 
+        Permission::create(['name' => 'dispositivo.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'dispositivo.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
+        Permission::create(['name' => 'dispositivo.destroy'])->syncRoles([$role1]);
+        
         //linea
-
-        Permission::create(['name' => 'linea.index'])->syncRoles([$role1, $role2, $role3]); //Cobranza 
-        Permission::create(['name' => 'linea.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'linea.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
+        Permission::create(['name' => 'linea.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza 
+        Permission::create(['name' => 'linea.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'linea.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
         Permission::create(['name' => 'linea.destroy'])->syncRoles([$role1]);
-        //sensor
 
-        Permission::create(['name' => 'sensor.index'])->syncRoles([$role1, $role2, $role3]); //Cobranza 
-        Permission::create(['name' => 'sensor.create'])->syncRoles([$role1, $role3]); //Cobranza  
-        Permission::create(['name' => 'sensor.edit'])->syncRoles([$role1, $role3]);  //Cobranza  
+        //sensor
+        Permission::create(['name' => 'sensor.index'])->syncRoles([$role1, $role2, $role3, $role4, $role5]); //Cobranza 
+        Permission::create(['name' => 'sensor.create'])->syncRoles([$role1, $role2]); //Cobranza  
+        Permission::create(['name' => 'sensor.edit'])->syncRoles([$role1, $role2]);  //Cobranza  
         Permission::create(['name' => 'sensor.destroy'])->syncRoles([$role1]);
+
+
+
+        Permission::create(['name' => 'crear.orden'])->syncRoles([$role1,$role3]);
+        Permission::create(['name' => 'crear.cita'])->syncRoles([$role1,$role3]);
+
+
+
+
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,89 +10,83 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::get('/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
-    Route::get('/', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+  Route::get('/home', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
+  Route::get('/', [App\Http\Controllers\AdminController::class, 'index'])->name('admin.index');
 
-    Route::resource('/user', App\Http\Controllers\UserController::class)->names('admin.users');
+  Route::resource('/user', App\Http\Controllers\UserController::class)->names('admin.users');
 
-    Route::get('/admin/edit/{user}', [App\Http\Controllers\UserController::class, 'edit'])->name('admin.edit');
-    Route::get('/admin/update/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('admin.update');
-    Route::get('/admin/destroy/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('admin.destroy');
-
-
-    Route::resource('referencia', App\Http\Controllers\ReferenciaController::class);
-    Route::resource('cliente',  App\Http\Controllers\ClienteController::class);
-    Route::get('/cliente/{id}',                 [App\Http\Controllers\ClienteController::class, 'show'])->name('cliente.show');
-    Route::get('cliente/{id}/buscararchivos',   [App\Http\Controllers\ClienteController::class, 'buscararchivos'])->name('buscar.buscararchivos');
-    Route::resource('vehiculo', App\Http\Controllers\VehiculoController::class);
-    Route::resource('linea',    App\Http\Controllers\LineaController::class);
-    Route::resource('cuenta',   App\Http\Controllers\CuentaController::class);
-    Route::resource('ctaespejo', App\Http\Controllers\CtaespejoController::class);
-    Route::resource('dispositivo', App\Http\Controllers\DispositivoController::class);
-    Route::resource('sensor', App\Http\Controllers\SensorController::class);
-
-    Route::get('/cliente/{id}',                 [App\Http\Controllers\ClienteController::class, 'show'])->name('cliente.show');
-    Route::get('cliente/{id}/buscararchivos',   [App\Http\Controllers\ClienteController::class, 'buscararchivos'])->name('buscar.buscararchivos');
-
-    Route::get('/referencia/crearref/{id}',     [App\Http\Controllers\ReferenciaController::class, 'crearref'])->name('referenciaf.crear');;
-    Route::post('/referencia/{id}',             [App\Http\Controllers\ReferenciaController::class, 'storef'])->name('referenciap.crear');
-
-    Route::get('/prueba/{id}/buscarVehiculo',   [App\Http\Controllers\PruebaController::class, 'buscarVehiculo'])->name('buscar.vehiculo');
-
-    Route::get('/prueba/{id}/buscarVehiculo',   [App\Http\Controllers\PruebaController::class, 'buscarVehiculo'])->name('buscar.vehiculo');
-    Route::get('/prueba/{id}/',   [App\Http\Controllers\PruebaController::class, 'buscadorvehiculo'])->name('buscador.vehiculo');
-
-    Route::get('/vehiculo/crearvehi/{id}',      [App\Http\Controllers\VehiculoController::class, 'crearvehi'])->name('vehiculof.crear');
-    Route::post('/vehiculo/{id}',               [App\Http\Controllers\VehiculoController::class, 'stovehi'])->name('vehiculop.crear');
-
-    Route::get('/prueba/{id}/buscarCuenta',     [App\Http\Controllers\PruebaController::class, 'buscarCuenta'])->name('buscar.cuenta');
-    Route::get('/cuenta/crearcta/{id}',         [App\Http\Controllers\CuentaController::class, 'crearcta'])->name('cuentaf.crear');
-    Route::post('/cuenta/{id}',                 [App\Http\Controllers\CuentaController::class, 'stocta'])->name('cuentap.crear');
-
-    Route::get('/prueba/{id}/buscarLinea',      [App\Http\Controllers\PruebaController::class, 'buscarLinea'])->name('buscar.linea');
-    Route::get('/linea/crearlinea/{id}',        [App\Http\Controllers\LineaController::class, 'crearlinea'])->name('lineaf.crear');
-    Route::post('/linea/{id}',                  [App\Http\Controllers\LineaController::class, 'storep'])->name('lineap.crear');
-
-    Route::get('/prueba/{id}/buscarDispositivo', [App\Http\Controllers\PruebaController::class, 'buscarDispositivo'])->name('buscar.dispositivo');
-    Route::get('/dispositivo/creardisp/{id}',   [App\Http\Controllers\DispositivoController::class, 'creardisp'])->name('dispositivof.crear');;
-    Route::post('/dispositivo/{id}',            [App\Http\Controllers\DispositivoController::class, 'stodis'])->name('dispositivop.crear');
-
-    Route::get('/prueba/{id}/buscarSensor',     [App\Http\Controllers\PruebaController::class, 'buscarSensor'])->name('buscar.sensor');
-    Route::get('/sensor/crearsens/{id}',   [App\Http\Controllers\SensorController::class, 'crearsens'])->name('sensorf.crear');;
-    Route::post('/sensor/{id}',            [App\Http\Controllers\SensorController::class, 'stosens'])->name('sensorp.crear');
-
-    Route::get('/prueba/{id}/buscarctaespejo',     [App\Http\Controllers\PruebaController::class, 'buscarCtaespejo'])->name('buscar.ctaespejo');
-    Route::get('/ctaespejo/crearctaespejo/{id}',   [App\Http\Controllers\CtaespejoController::class, 'crearctaespejo'])->name('ctaesoejof.crear');;
-    Route::post('/ctaespejo/{id}',            [App\Http\Controllers\CtaespejoController::class, 'storectaespejo'])->name('ctaespejop.crear');
-
-    Route::get('/crear',   [App\Http\Controllers\ClienteController::class, 'crearcliente'])->name('crear.nuevo');
-    Route::post('/create',   [App\Http\Controllers\ClienteController::class, 'createnuevo'])->name('create.nuevo');
-
-    Route::get('/crearr/{id}',   [App\Http\Controllers\ReferenciaController::class, 'crearr'])->name('crear.nuevo.ref');
-    Route::post('/create/{id}',   [App\Http\Controllers\ReferenciaController::class, 'createnuevoref'])->name('create.nuevo.ref');
-
-    Route::get('/crearc/{id}',   [App\Http\Controllers\CuentaController::class, 'crearc'])->name('crear.nuevo.cuenta');
-    Route::post('/createc/{id}',   [App\Http\Controllers\CuentaController::class, 'createnuevocta'])->name('create.nuevo.cta');
+  Route::get('/admin/edit/{user}', [App\Http\Controllers\UserController::class, 'edit'])->name('admin.edit');
+  Route::get('/admin/update/{user}', [App\Http\Controllers\UserController::class, 'update'])->name('admin.update');
+  Route::get('/admin/destroy/{user}', [App\Http\Controllers\UserController::class, 'destroy'])->name('admin.destroy');
 
 
+  Route::resource('referencia', App\Http\Controllers\ReferenciaController::class);
+  Route::resource('cliente',  App\Http\Controllers\ClienteController::class);
+  Route::get('/cliente/{id}',                 [App\Http\Controllers\ClienteController::class, 'show'])->name('cliente.show');
+  Route::get('cliente/{id}/buscararchivos',   [App\Http\Controllers\ClienteController::class, 'buscararchivos'])->name('buscar.buscararchivos');
+  Route::resource('vehiculo', App\Http\Controllers\VehiculoController::class);
+  Route::resource('linea',    App\Http\Controllers\LineaController::class);
+  Route::resource('cuenta',   App\Http\Controllers\CuentaController::class);
+  Route::resource('ctaespejo', App\Http\Controllers\CtaespejoController::class);
+  Route::resource('dispositivo', App\Http\Controllers\DispositivoController::class);
+  Route::resource('sensor', App\Http\Controllers\SensorController::class);
 
-    Route::get('/crearcita/{vehiculo} ',   [App\Http\Controllers\ClienteController::class, 'crearcita'])->name('crear.cita');
-    Route::post('/fechaservicio',   [App\Http\Controllers\ClienteController::class, 'fechaservicio'])->name('fechaservicio');
+  Route::get('/cliente/{id}',                 [App\Http\Controllers\ClienteController::class, 'show'])->name('cliente.show');
+  Route::get('cliente/{id}/buscararchivos',   [App\Http\Controllers\ClienteController::class, 'buscararchivos'])->name('buscar.buscararchivos');
 
+  Route::get('/referencia/crearref/{id}',     [App\Http\Controllers\ReferenciaController::class, 'crearref'])->name('referenciaf.crear');;
+  Route::post('/referencia/{id}',             [App\Http\Controllers\ReferenciaController::class, 'storef'])->name('referenciap.crear');
+
+  Route::get('/prueba/{id}/buscarVehiculo',   [App\Http\Controllers\PruebaController::class, 'buscarVehiculo'])->name('buscar.vehiculo');
+
+  Route::get('/prueba/{id}/buscarVehiculo',   [App\Http\Controllers\PruebaController::class, 'buscarVehiculo'])->name('buscar.vehiculo');
+  Route::get('/prueba/{id}/',   [App\Http\Controllers\PruebaController::class, 'buscadorvehiculo'])->name('buscador.vehiculo');
+
+  Route::get('/vehiculo/crearvehi/{id}',      [App\Http\Controllers\VehiculoController::class, 'crearvehi'])->name('vehiculof.crear');
+  Route::post('/vehiculo/{id}',               [App\Http\Controllers\VehiculoController::class, 'stovehi'])->name('vehiculop.crear');
+
+  Route::get('/prueba/{id}/buscarCuenta',     [App\Http\Controllers\PruebaController::class, 'buscarCuenta'])->name('buscar.cuenta');
+  Route::get('/cuenta/crearcta/{id}',         [App\Http\Controllers\CuentaController::class, 'crearcta'])->name('cuentaf.crear');
+  Route::post('/cuenta/{id}',                 [App\Http\Controllers\CuentaController::class, 'stocta'])->name('cuentap.crear');
+
+  Route::get('/prueba/{id}/buscarLinea',      [App\Http\Controllers\PruebaController::class, 'buscarLinea'])->name('buscar.linea');
+  Route::get('/linea/crearlinea/{id}',        [App\Http\Controllers\LineaController::class, 'crearlinea'])->name('lineaf.crear');
+  Route::post('/linea/{id}',                  [App\Http\Controllers\LineaController::class, 'storep'])->name('lineap.crear');
+
+  Route::get('/prueba/{id}/buscarDispositivo', [App\Http\Controllers\PruebaController::class, 'buscarDispositivo'])->name('buscar.dispositivo');
+  Route::get('/dispositivo/creardisp/{id}',   [App\Http\Controllers\DispositivoController::class, 'creardisp'])->name('dispositivof.crear');;
+  Route::post('/dispositivo/{id}',            [App\Http\Controllers\DispositivoController::class, 'stodis'])->name('dispositivop.crear');
+
+  Route::get('/prueba/{id}/buscarSensor',     [App\Http\Controllers\PruebaController::class, 'buscarSensor'])->name('buscar.sensor');
+  Route::get('/sensor/crearsens/{id}',   [App\Http\Controllers\SensorController::class, 'crearsens'])->name('sensorf.crear');;
+  Route::post('/sensor/{id}',            [App\Http\Controllers\SensorController::class, 'stosens'])->name('sensorp.crear');
+
+  Route::get('/prueba/{id}/buscarctaespejo',     [App\Http\Controllers\PruebaController::class, 'buscarCtaespejo'])->name('buscar.ctaespejo');
+  Route::get('/ctaespejo/crearctaespejo/{id}',   [App\Http\Controllers\CtaespejoController::class, 'crearctaespejo'])->name('ctaesoejof.crear');;
+  Route::post('/ctaespejo/{id}',            [App\Http\Controllers\CtaespejoController::class, 'storectaespejo'])->name('ctaespejop.crear');
+
+  Route::get('/crear',   [App\Http\Controllers\ClienteController::class, 'crearcliente'])->name('crear.nuevo');
+  Route::post('/create',   [App\Http\Controllers\ClienteController::class, 'createnuevo'])->name('create.nuevo');
+
+  Route::get('/crearr/{id}',   [App\Http\Controllers\ReferenciaController::class, 'crearr'])->name('crear.nuevo.ref');
+  Route::post('/create/{id}',   [App\Http\Controllers\ReferenciaController::class, 'createnuevoref'])->name('create.nuevo.ref');
+
+  Route::get('/crearc/{id}',   [App\Http\Controllers\CuentaController::class, 'crearc'])->name('crear.nuevo.cuenta');
+  Route::post('/createc/{id}',   [App\Http\Controllers\CuentaController::class, 'createnuevocta'])->name('create.nuevo.cta');
+
+
+//click hacia seleccionar fecha de servicio
+  Route::get('/crearcita/{vehiculo} ',   [App\Http\Controllers\ClienteController::class, 'crearcita'])->name('crear.cita');
+  //orden de servicio recibe datos del vehiculo
+  Route::get('/orden/{vehiculoid}', [App\Http\Controllers\ClienteController::class, 'orden'])->name('crear.ordens');
 
 
   //orden de instalacion
-    Route::get('/orden',      [App\Http\Controllers\ClienteController::class, 'orden'])->name('crear.orden');
-    
-    Route::get('/ordeninstalacion',   [App\Http\Controllers\ClienteController::class, 'ordeninstalacion'])->name('ordeninstalacion');
-    Route::post('/ordenins',   [App\Http\Controllers\ClienteController::class, 'ordenins'])->name('ordenins');
+  Route::get('/orden',      [App\Http\Controllers\ClienteController::class, 'orden'])->name('crear.orden'); //admin envia a form
+  Route::get('/ordeninstalacion',   [App\Http\Controllers\ClienteController::class, 'ordeninstalacion'])->name('ordeninstalacion'); //envia la informacion al store 
+  Route::post('/ordenins',   [App\Http\Controllers\ClienteController::class, 'ordenins'])->name('ordenins'); //generar el pdf con la informacion 
 
-    //orden de servicio recibe datos del vehiculo
-    Route::get('/orden/{vehiculoid}', [App\Http\Controllers\ClienteController::class, 'orden'])->name('crear.ordens');
-
-
-    Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
-    Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
-
-    
+//crear usuarios desde user
+  Route::get('/usuarios/create', [UsuarioController::class, 'create'])->name('usuarios.create');
+  Route::post('/usuarios', [UsuarioController::class, 'store'])->name('usuarios.store');
 });
