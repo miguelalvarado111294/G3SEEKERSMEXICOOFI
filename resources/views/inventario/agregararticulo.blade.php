0aa9dc7f6d9e3485c90d31bbf42ad72a3ece7ded
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-11-12 10:22:07 -0600

    inventario de dispositivo y lineas ok

--- resources/views/inventario/agregararticulo.blade.php
+++ resources/views/inventario/agregararticulo.blade.php
@@ -10,14 +10,21 @@
 @section('content')
     <!-- Mostrar mensajes de éxito o error -->
     @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @elseif(session('error'))
-        <div class="alert alert-danger">
-            {{ session('error') }}
-        </div>
-    @endif
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+        {{ session('success') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@elseif(session('error'))
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+        {{ session('error') }}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
+@endif
+
 
     <form id="registroForm" action="{{ route('store.inventario') }}" method="POST">
         @csrf
