5030b03674f2a2254ea39114681a9d2548b00054
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-11-19 12:18:21 -0600

    modelo con mayus  okx3

--- app/Models/Linea.php
+++ app/Models/Linea.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class linea extends Model
+class Linea extends Model
 {
     use HasFactory;
 
