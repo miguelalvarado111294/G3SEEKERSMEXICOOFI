90f38060cb5449e2d80b2ba4c9d27bc39d4c8efb
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-11-20 13:07:10 -0600

    index ref

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -6,12 +6,24 @@
     <h1 class="text-center"><b>G3 Seekers México</b></h1>
 @stop
 
+
+
+
 @section('css')
-    <!-- AdminLTE y dependencias por CDN -->
+    <!-- AdminLTE CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    
+    <!-- Font Awesome (iconos) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    
+    <!-- Bootstrap CSS (dependencia para la estructura y componentes) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    
+    <!-- jQuery UI CSS (opcional, si usas componentes interactivos) -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
 @endsection
+
+
 
 @section('content')
     <div class="container">
@@ -89,15 +101,24 @@
     </div>
 @endsection
 
+
+
+
+
 @section('js')
     <!-- jQuery y Popper por CDN -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    
+    <!-- Popper.js (necesario para Bootstrap) -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
         integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
-    <!-- Bootstrap y AdminLTE por CDN -->
+    <!-- Bootstrap JS (para interactividad de componentes como botones, menús, etc.) -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    
+    <!-- AdminLTE JS (funcionalidad para los elementos de AdminLTE) -->
     <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
 
     <!-- Scripts adicionales -->
     <script>
--- resources/views/cliente/show.blade.php
+++ resources/views/cliente/show.blade.php
@@ -8,6 +8,27 @@
 
 @endsection
 
+@section('css')
+    <!-- AdminLTE CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    
+    <!-- Font Awesome (iconos) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    
+    <!-- Bootstrap CSS (dependencia para la estructura y componentes) -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    
+    <!-- jQuery UI CSS (opcional, si usas componentes interactivos) -->
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
+@endsection
+
+
+
+
+
+
+
+
 @section('content')
     <div class="container">
         <div class="card">
@@ -29,18 +50,21 @@
                         <b>Comentarios:</b> {{ $cliente->comentarios }} <br>
                     </div>
                 </div>
-                
+
                 <hr>
                 <div class="d-flex justify-content-center">
-                    <a href="{{ route('buscar.buscararchivos', $cliente->id) }}" class="btn btn-primary m-2" style="width: 17%;">Documentos electrónicos</a>
-                    <a href="{{ route('buscar.cuenta', $cliente->id) }}" class="btn btn-primary m-2" style="width: 17%;">Datos Adicionales</a>
+                    <a href="{{ route('buscar.buscararchivos', $cliente->id) }}" class="btn btn-primary m-2"
+                        style="width: 17%;">Documentos electrónicos</a>
+                    <a href="{{ route('buscar.cuenta', $cliente->id) }}" class="btn btn-primary m-2"
+                        style="width: 17%;">Datos Adicionales</a>
                 </div>
 
                 <br>
 
                 @can('cliente.edit')
                     <div class="text-center">
-                        <a href="{{ url('/cliente/' . $cliente->id . '/edit') }}" class="btn btn-warning m-2" style="width: 17%;">Editar</a>
+                        <a href="{{ url('/cliente/' . $cliente->id . '/edit') }}" class="btn btn-warning m-2"
+                            style="width: 17%;">Editar</a>
                     </div>
                 @endcan
 
@@ -49,7 +73,8 @@
                         <form action="{{ url('/cliente/' . $cliente->id) }}" method="post" class="d-inline">
                             @csrf
                             {{ method_field('DELETE') }}
-                            <input class="btn btn-danger m-2" style="width: 17%;" type="submit" onclick=" return confirm('seguro quieres eliminar?')" value="Borrar">
+                            <input class="btn btn-danger m-2" style="width: 17%;" type="submit"
+                                onclick=" return confirm('seguro quieres eliminar?')" value="Borrar">
                         </form>
                     </div>
                 @endcan
@@ -63,7 +88,8 @@
             <div class="card-body">
                 @can('referencia.create')
                     <div class="text-center mb-3">
-                        <a href="{{ route('referenciaf.crear', $cliente->id) }}" class="btn btn-success">Registrar Nueva Referencia</a>
+                        <a href="{{ route('referenciaf.crear', $cliente->id) }}" class="btn btn-success">Registrar Nueva
+                            Referencia</a>
                     </div>
                 @endcan
 
@@ -94,14 +120,17 @@
                                 <td>{{ $referencia->comentarios }}</td>
                                 <td>
                                     @can('referencia.edit')
-                                        <a href="{{ url('/referencia/' . $referencia->id . '/edit') }}" class="btn btn-warning">Editar</a>
+                                        <a href="{{ url('/referencia/' . $referencia->id . '/edit') }}"
+                                            class="btn btn-warning">Editar</a>
                                     @endcan
 
                                     @can('referencia.destroy')
-                                        <form action="{{ url('/referencia/' . $referencia->id) }}" method="post" class="d-inline">
+                                        <form action="{{ url('/referencia/' . $referencia->id) }}" method="post"
+                                            class="d-inline">
                                             @csrf
                                             {{ method_field('DELETE') }}
-                                            <input class="btn btn-danger" type="submit" onclick="return confirm('seguro quieres eliminar?')" value="Borrar">
+                                            <input class="btn btn-danger" type="submit"
+                                                onclick="return confirm('seguro quieres eliminar?')" value="Borrar">
                                         </form>
                                     @endcan
                                 </td>
@@ -116,4 +145,22 @@
             <a href="{{ url('cliente') }}" class="btn btn-dark">Regresar</a>
         </div>
     </div>
+@endsection
+
+
+
+@section('js')
+    <!-- jQuery (necesario para AdminLTE y otros componentes) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+
+    <!-- Popper.js (necesario para Bootstrap) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+
+    <!-- Bootstrap JS (para interactividad de componentes como botones, menús, etc.) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- AdminLTE JS (funcionalidad para los elementos de AdminLTE) -->
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
 @endsection
