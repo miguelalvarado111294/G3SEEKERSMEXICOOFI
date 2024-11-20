9b4fcd26d85de95348b6d698e57b4315b5ed4b6e
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-11-20 10:04:47 -0600

    cdn en index cliente test

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -4,11 +4,13 @@
 
 @section('content_header')
     <h1 class="text-center"><b>G3 Seekers México</b></h1>
-
 @stop
 
 @section('css')
-<link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
+    <!-- AdminLTE y dependencias por CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
 @endsection
 
 @section('content')
@@ -17,7 +19,7 @@
             <div class="alert alert-success alert-dismissible" role="alert">
                 {{ Session::get('mensaje') }}
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true"> &times </span>
+                    <span aria-hidden="true">&times;</span>
                 </button>
             </div>
         @endif
@@ -35,24 +37,23 @@
                     <input type="text" name="search" id="search"
                         placeholder="Para Buscar un Cliente, Ingrese Nombre / Apellido / Teléfono / Email / RFC"
                         class="form-control" onfocus="this.value=''">
-        
+
                     <div id="search_list"></div>
                 </div>
             </div>
         </div>
-        
+
         <style>
             /* Centrar el texto del placeholder */
             #search::placeholder {
                 text-align: center;
             }
-        
+
             /* Opcional: puedes personalizar también el estilo del input */
             #search {
                 text-align: center; /* Centrar el texto ingresado */
             }
         </style>
-        
 
         <br>
         <div class="card">
@@ -89,12 +90,16 @@
 @endsection
 
 @section('js')
+    <!-- jQuery y Popper por CDN -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
-        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
-    </script>
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
-    <!-- Scripts de AdminLTE -->
+    <!-- Bootstrap y AdminLTE por CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
+    <!-- Scripts adicionales -->
     <script>
         $(document).ready(function() {
             $('#search').on('keyup', function() {
@@ -104,7 +109,6 @@
                     // Limpiar la lista si el input está vacío
                     $('#search_list').html('');
                 } else {
-
                     $.ajax({
                         url: "search",
                         type: "GET",
@@ -116,8 +120,6 @@
                         }
                     });
                 }
-
-                //end of ajax call
             });
         });
     </script>
