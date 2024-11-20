79176f2bde1828bf692932f981bef0f9824b1026
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-11-20 13:48:09 -0600

    index adminnn

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -13,16 +13,15 @@
     <!-- Font Awesome (iconos) -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
 
-    <!-- Bootstrap CSS (dependencia para la estructura y componentes) -->
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
-    <!-- jQuery UI CSS (opcional, si usas componentes interactivos) -->
+    <!-- jQuery UI CSS -->
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.min.css">
 @endsection
 
-
 @section('content')
-    <p class="text-center">Bienvenido, <strong>{{ Auth::user()->name }}</strong>, al Panel de Control</p>
+    <p class="text-center">Bienvenido, <strong>{{ Auth::check() ? Auth::user()->name : 'Invitado' }}</strong>, al Panel de Control</p>
 
     <div class="card my-4">
         <div class="card-body">
@@ -33,12 +32,21 @@
     </div>
 @stop
 
-@section('css')
-    {{-- Add here extra stylesheets --}}
-    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-@stop
-
 @section('js')
+    <!-- jQuery -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+
+    <!-- Popper.js (necesario para Bootstrap) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" 
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- AdminLTE JS -->
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+
     <script>
         console.log("Hi, I'm using the Laravel-AdminLTE package!");
     </script>
