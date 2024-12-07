3d6f5947d97c3895e72f784998749729f8ae88a4
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2024-12-07 13:06:32 -0600

    post

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\UsuarioController;
 Auth::routes(['register' => false, 'reset' => false]);
 
 Route::get('/clientes/crear', [App\Http\Controllers\ClienteController::class, 'crearcliente'])->name('crear.nuevo');
-Route::get('/clientes/create', [App\Http\Controllers\ClienteController::class, 'createnuevo'])->name('create.nuevo');
+Route::post('/clientes/create', [App\Http\Controllers\ClienteController::class, 'createnuevo'])->name('create.nuevo');
 
 Route::get('/referencias/crearr/{id}', [App\Http\Controllers\ReferenciaController::class, 'crearr'])->name('crear.nuevo.ref');
 Route::post('/referencias/create/{id}', [App\Http\Controllers\ReferenciaController::class, 'createnuevoref'])->name('create.nuevo.ref');
