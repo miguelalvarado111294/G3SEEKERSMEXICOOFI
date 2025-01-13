b86f604080ccdb6f035061ea1b923bef06247466
miguelalvarado111294 <97652509+miguelalvarado111294@users.noreply.github.com>
2025-01-13 11:31:25 -0600

    https a httpss

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // Añadir esta línea
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // Forzar el uso de HTTPS
         if (env('APP_ENV') !== 'local') {
-            \URL::forceScheme('https');
+            URL::forceScheme('https'); // Usar URL::forceScheme
         }
 
         // Configurar la paginación con Bootstrap
